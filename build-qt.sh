712f8a3a875905f816e011f811fac43f0feec1dd
winston <hey@winston.sh>
2021-12-25 06:54:05 +0100

    clean up actions, hopefully make build-qt more portable

--- build-qt.sh
+++ build-qt.sh
@@ -3,17 +3,18 @@
 LANGS=("de")
 
 for lang in "${LANGS[@]}"; do
-  pyside6-lupdate src/ui/*.ui -ts src/i18n/$lang.ts
+  pyside6-lupdate src/ui/*.ui -ts "src/i18n/${lang}.ts"
 done
 
 mkdir -p src/res/i18n
-find src/i18n -name "*.ts" -exec bash -c '
-  fname="${1%.ts}"
-  pyside6-lrelease "$1" -qm "${fname/src\/i18n\//src\/res\/i18n\/}.qm"
-' bash {} \;
+for file in src/i18n/*.ts; do
+  fname=$(echo "${file%.ts}" | sed 's/^src\/i18n\///')
+  pyside6-lrelease "$file" -qm "src/res/i18n/${fname}.qm"
+done
 
-find src/ui -name "*.ui" -exec bash -c '
-  pyside6-uic "$1" -o "${1%.ui}.py"
-' bash {} \;
+# shellcheck disable=SC2044
+for file in $(find src/ui -name '*.ui'); do
+  pyside6-uic "$file" -o "${file%.ui}.py"
+done
 
 pyside6-rcc src/res/resources.qrc -o src/res/__init__.py
