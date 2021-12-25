05fbe1ce8c675c36e174d8f714405c34a717197a
winston <hey@winston.sh>
2021-12-25 22:25:07 +0100

    build: add make.py to replace build-qt.sh

---
+++ make.py
@@ -0,0 +1,78 @@
+import glob
+import os
+import pathlib
+import sys
+from os import path
+from subprocess import Popen, PIPE
+
+import PySide6
+
+
+# modified qt_tool_wrapper from PySide6.scripts
+def qtt(qt_tool, args, libexec=False):
+    pyside_dir = pathlib.Path(PySide6.__file__).resolve().parent
+    if libexec and sys.platform != "win32":
+        exe = pyside_dir / "Qt" / "libexec" / qt_tool
+    else:
+        exe = pyside_dir / qt_tool
+    cmd = [os.fspath(exe)] + args
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+
+    if err:
+        msg = err.decode("utf-8")
+        command = " ".join(cmd)
+        print(f"Error: {msg}\nwhile executing '{command}'")
+        sys.exit(1)
+
+    if out.decode('utf-8') != "":
+        print(out.decode("utf-8"))
+
+
+def run_lupdate(languages):
+    print("lupdate:")
+    for lang in languages:
+        for file in glob.glob("src/**/*.ui", recursive=True):
+            dest = "src/i18n/{}.ts".format(lang)
+            qtt("lupdate", [file, "-ts", dest])
+
+
+def run_lrelease():
+    print("lrelease:")
+    destpath = path.join("src", "res", "i18n")
+
+    if not os.path.isdir(destpath):
+        os.mkdir("src/res/i18n")
+
+    for file in glob.glob("src/i18n/*.ts"):
+        language = path.splitext(path.basename(file))[0]
+        dest = path.join(destpath, "{}.qm".format(language))
+        qtt("lrelease", [file, "-qm", dest])
+
+
+def run_uic():
+    for file in glob.glob("src/ui/**/*.ui", recursive=True):
+        dest = "{}.py".format(path.splitext(file)[0])
+        print("uic: {} -> {}".format(file, dest))
+        qtt("uic", ["-g", "python", file, "-o", dest], True)
+
+
+def run_rcc():
+    file = "src/res/resources.qrc"
+    dest = "src/res/__init__.py"
+    print("rcc: {} -> {}".format(file, dest))
+    qtt("rcc", ["-g", "python", file, "-o", dest], True)
+
+
+if __name__ == "__main__":
+    # supported languages for i18n
+    languages = ["de"]
+
+    # update files for linguist
+    run_lupdate(languages)
+    # compile translation files
+    run_lrelease()
+    # compile designer files to python code
+    run_uic()
+    # finally, make python resource file
+    run_rcc()
