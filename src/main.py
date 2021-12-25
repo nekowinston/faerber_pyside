3290ee2fdd526ff217715afb1e08e4673c58ffe7
winston <hey@winston.sh>
2021-12-25 04:17:51 +0100

    improve string replacement for i18n

--- src/main.py
+++ src/main.py
@@ -19,7 +19,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.lbl_open_image.click.connect(self.open_file_dialog.open)
         self.lbl_open_image.wrongdrop.connect(self.show_drop_decline)
-        self.initialText = self.lbl_open_image.text()
+        self.lbl_initialText = self.lbl_open_image.text()
+        self.lbl_rejectText = self.lbl_open_image.property("dropRejectText")
 
         self.open_file_dialog.fileSelected.connect(self.load_image)
         self.lbl_open_image.drop.connect(self.load_image)
@@ -30,15 +31,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     # change label text, to indicate that the file is wrong
     def show_drop_decline(self, event):
         if event:
-            self.lbl_open_image.setText(
-                "<html><head/><body><p align='center'><span style='font-size:14pt;'>Please drop a <strong>single</strong> PNG or JPEG</span></p></body></html>"
-            )
+            self.lbl_open_image.setText(self.lbl_rejectText)
         else:
-            self.lbl_open_image.setText(self.initialText)
+            self.lbl_open_image.setText(self.lbl_initialText)
 
     # reset the label text, if it was changed by the drop event
     def leaveEvent(self, event):
-        if self.lbl_open_image.text() != self.initialText:
+        if self.lbl_open_image.text() != self.lbl_initialText:
             self.show_drop_decline(False)
 
 
