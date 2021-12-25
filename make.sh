7535ab79e7908eac127eea43d5251db58004d6e3
winston <hey@winston.sh>
2021-12-25 23:28:37 +0100

    build: remove deprecated bash scripts

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# use hash seed for reproducibility
-export PYTHONHASHSEED=2147483647
-
-pyinstaller .pyinstaller/build.spec
-
-# let Python be unpredictable again (default)
-unset PYTHONHASHSEED
