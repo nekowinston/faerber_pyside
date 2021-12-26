af75dee0a98ceba1546bad145c985916d0033ccb
winston <hey@winston.sh>
2021-12-26 17:54:55 +0100

    feat: add some more usability, connect more widgets

--- src/PreferencesWindow.py
+++ src/PreferencesWindow.py
@@ -1,9 +1,17 @@
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import QFile, QJsonDocument
+from PySide6.QtWidgets import QMainWindow
+
 from ui.Preferences import Ui_Preferences
 
 
 # TODO: make this actually useful
-class PreferencesWindow(QWidget, Ui_Preferences):
+class PreferencesWindow(QMainWindow, Ui_Preferences):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # load default color schemes
+        rc = QFile(":/data/colorschemes.json")
+        rc.open(QFile.ReadOnly)
+        self.colorschemes = QJsonDocument.fromJson(rc.readAll().data())
+        self.cbb_colorscheme.addItems(self.colorschemes.object().keys())
--- src/utils/load_stylesheet.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtCore import QFile
-
-
-def load_stylesheet(self, res=":/styles/dracula"):
-    rc = QFile(res)
-    rc.open(QFile.ReadOnly)
-    content = rc.readAll().data()
-    self.setStyleSheet(str(content, "utf-8"))
