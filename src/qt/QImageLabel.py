356cfa8cc7cecfdc67c1309c2b99fc90d13802ce
winston <hey@winston.sh>
2021-12-26 04:07:38 +0100

    fix: constrain QImageLabel size also on the initial draw

--- src/qt/QImageLabel.py
+++ src/qt/QImageLabel.py
@@ -10,23 +10,26 @@ class QImageLabel(QLabel):
         # store original pixmap, to preserve scaling quality
         self.orig_pixmap = QPixmap()
 
-    def resizeEvent(self, event: QResizeEvent) -> None:
-        # is the new scale > the image size? if so, cap at orignal size
-        if self.orig_pixmap.width() < event.size().width():
-            self.setPixmap(self.orig_pixmap, self.orig_pixmap.size())
-
-        # otherwise, scale down to 0, 0
+    def __capped_size(self, size):
+        # is the new scale > the image size? return the orignal size
+        if self.orig_pixmap.width() < size.width():
+            return self.orig_pixmap.size()
+        # otherwise, scale down to size
         else:
-            self.setPixmap(self.orig_pixmap, event.size())
+            return size
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        self.setPixmap(self.orig_pixmap, self.__capped_size(event.size()))
 
     def setPixmap(self, pixmap: QPixmap, size: QSize = QSize()) -> None:
         # default to self.frameSize()
         if size.isEmpty():
             size = self.frameSize()
+            pass
 
         # store the passed pixmap as the original
         self.orig_pixmap = pixmap
         # scale it
-        pixmap_scaled = pixmap.scaled(size, Qt.KeepAspectRatio)
+        pixmap_scaled = pixmap.scaled(self.__capped_size(size), Qt.KeepAspectRatio)
         # and use the inherited function to set the actual pixmap
         super(QImageLabel, self).setPixmap(pixmap_scaled)
