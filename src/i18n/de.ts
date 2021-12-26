e88fdb5569b80dee31ddbab08a1b2f45793f8447
winston <hey@winston.sh>
2021-12-26 05:52:27 +0100

    feat: add basic non-interactive preferences window

--- src/i18n/de.ts
+++ src/i18n/de.ts
@@ -4,94 +4,112 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../ui/MainWindow.ui" line="20"/>
-        <source>IGNQt</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.ui" line="61"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt;&quot;&gt;Click inside to open the Filebrowser&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;or drop an Image anywhere&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt;&quot;&gt;Klicken Sie hier, um den Filebrowser zu öffnen&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;oder legen Sie hier ein Bild ab&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:14pt;&quot;&gt;Klicken Sie hier, um den Filebrowser zu öffnen&lt;/span&gt;&lt;/p&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:10pt;&quot;&gt;oder legen Sie hier ein Bild ab&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="64"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&apos;center&apos;&gt;&lt;span style=&apos;font-size:14pt;&apos;&gt;Please drop a &lt;strong&gt;single&lt;/strong&gt; PNG or JPEG&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&apos;center&apos;&gt;&lt;span style=&apos;font-size:14pt;&apos;&gt;Bitte legen Sie ein &lt;strong&gt;einzelnes&lt;/strong&gt; PNG oder JPEG ab&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="vanished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&apos;center&apos;&gt;&lt;span style=&apos;font-size:14pt;&apos;&gt;Bitte legen Sie ein &lt;strong&gt;einzelnes&lt;/strong&gt; PNG oder JPEG ab&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="109"/>
         <source>Compare</source>
-        <translation>Vergleichen</translation>
+        <translation type="vanished">Vergleichen</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="130"/>
         <source>File</source>
-        <translation>Datei</translation>
+        <translation type="vanished">Datei</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="138"/>
         <source>Edit</source>
-        <translation>Bearbeiten</translation>
+        <translation type="vanished">Bearbeiten</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="144"/>
         <source>Help</source>
-        <translation>Hilfe</translation>
+        <translation type="vanished">Hilfe</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="155"/>
         <source>Quit</source>
-        <translation>Schließen</translation>
+        <translation type="vanished">Schließen</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="158"/>
         <source>Ctrl+Q</source>
-        <translation>Strg+Q</translation>
+        <translation type="vanished">Strg+Q</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="166"/>
         <source>Save</source>
-        <translation>Speichern</translation>
+        <translation type="vanished">Speichern</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="169"/>
         <source>Ctrl+S</source>
-        <translation>Strg+S</translation>
+        <translation type="vanished">Strg+S</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="174"/>
         <source>Preferences</source>
-        <translation>Eigenschaften</translation>
+        <translation type="vanished">Eigenschaften</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="177"/>
         <source>Ctrl+,</source>
-        <translation>Strg+,</translation>
+        <translation type="vanished">Strg+,</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="182"/>
-        <source>Support</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.ui" line="185"/>
-        <source>F1</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="../ui/MainWindow.ui" line="190"/>
         <source>About</source>
-        <translation>Über</translation>
+        <translation type="vanished">Über</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="195"/>
         <source>Open Image</source>
-        <translation>Bild öffnen</translation>
+        <translation type="vanished">Bild öffnen</translation>
     </message>
     <message>
-        <location filename="../ui/MainWindow.ui" line="198"/>
         <source>Ctrl+O</source>
-        <translation>Strg+O</translation>
+        <translation type="vanished">Strg+O</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="../ui/Preferences.ui" line="14"/>
+        <source>Preferences</source>
+        <translation type="unfinished">Eigenschaften</translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="26"/>
+        <source>Image Generation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="43"/>
+        <source>Colorscheme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="53"/>
+        <source>Nord</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="62"/>
+        <source>Blur output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="69"/>
+        <source>Average pixels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="124"/>
+        <source>Colorscheme Editor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="149"/>
+        <source>Add color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/Preferences.ui" line="156"/>
+        <source>Delete selected color</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
